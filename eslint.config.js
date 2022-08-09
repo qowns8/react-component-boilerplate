d12bb3bc3372c0b98a4d68a920f86ac5459a64b9
willy <qowns8@madup.com>
2022-08-09 14:20:49 +0900

    setting lint, prettier

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        rules: {
+            semi: "error",
+            "prefer-const": "error",
+            "require-await": "error",
+            "curly": "warn",
+            "jex-quotes": "error",
+            'no-empty': 'error',
+            "no-unused-vars": "off",
+        }
+    }
+]
