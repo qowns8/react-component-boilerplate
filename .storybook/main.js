c7559358e5445b63d1c757cc6763cd2639dff1b0
willy <qowns8@madup.com>
2022-08-29 18:42:13 +0900

    storybook default setting

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "stories": [
+    "../packages/docs/**/*.stories.mdx",
+    "../packages/docs/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react"
+}
