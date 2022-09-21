a02a7dffa84c70e6804544df053032d1ee570614
willy <qowns8@madup.com>
2022-09-21 23:34:37 +0900

    config lint-staged and husky

--- packages/react-component/src/index.test.tsx
+++ packages/react-component/src/index.test.tsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react'
+import React from 'react'
 import Component from './index'
 
 test('renders learn react link', () => {
-  render(<Component />)
+  render(<Component headers={[]} />)
   screen.getByText('그래야만')
-});
+})
