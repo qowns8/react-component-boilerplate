64c64fab3f6f077a9b8750bedba0a45a872c28d3
willy <qowns8@madup.com>
2022-09-21 23:35:01 +0900

    check commit

--- packages/react-component/src/index.tsx
+++ packages/react-component/src/index.tsx
@@ -1,12 +1,15 @@
 import './index.scss'
-import {ITableProps} from "./types";
+import { ITableProps } from './types'
 import Header from './header'
-import TableBody from "./body";
-import React from 'react';
+import TableBody from './body'
+import React from 'react'
 
-export default function Component (props: ITableProps) {
-  return <div className="lehkost-table-wrappper">
-    <Header headers={props.headers}/>
-    <TableBody data={props.data} headerProps={props.headers}/>
-  </div>
+export default function Component(props: ITableProps) {
+  return (
+    <div className="lehkost-table-wrappper">
+      <Header headers={props.headers} />
+
+      <TableBody data={props.data} headerProps={props.headers} />
+    </div>
+  )
 }
